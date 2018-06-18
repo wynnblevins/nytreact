60535331dd662e853dd4b67a18308c175d589dfc
Wynn Blevins <wynnblevins@yahoo.com>
2018-06-18 16:41:19 -0400

    Added remove note method to save articles page, button currently does nothing but log a message to the console.

--- client/src/components/SavedArticle.js
+++ client/src/components/SavedArticle.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Button } from 'react-materialize';
 
 class SavedArticle extends Component {
   constructor(props) {
@@ -6,10 +7,24 @@ class SavedArticle extends Component {
     this.state = {};
   }
   
+  onAddNoteClick = () => {
+    console.log('inside add note method.');
+  }
+
+  onNoteDeleteClick = () => {
+    console.log('inside delete note method.');
+  }
+
   render = () => {
     return (
       <div className="scrapedHeadline">
         <h2>{this.props.name}</h2>
+        <Button onClick={() => {
+          this.onAddNoteClick();
+        }} waves='light'>Add Note</Button>
+        <Button variant="contained" color="secondary" onClick={() => {
+          this.onNoteDeleteClick();
+        }}>Remove</Button>
       </div>
     );
   }
